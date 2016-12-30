e78c90e46114d3f7fd9a8f22584fbd67ce377cb6
Michael Mueller <mdmueller3@wisc.edu>
2016-12-29 18:14:18 -0600

    header modifications

--- pages/index.js
+++ pages/index.js
@@ -41,8 +41,12 @@ export default class Page extends Component {
         <div className='w-100 center'>
           <Header />
 
-          <div className = "w-100 db contain bg-center" style = {{"backgroundImage":"url(/static/berlin.jpg)", "paddingBottom":"49.2%"}}>
-            
+          <div className = "w-100 db contain bg-center layout horizontal center" style = {{"backgroundImage":"url(/static/berlin.jpg)"}}>
+            <div className="center">
+              <h1 className = "white mt0">Entdecke die Welt!</h1>
+              <h1 className = "white mt0">Entdecke die Welt!</h1>
+            </div>
+            <div style={{"paddingBottom":"50%"}} />
           </div>
 
           {/* <img className="w-100 db" src="/static/berlin.jpg" /> /*}
