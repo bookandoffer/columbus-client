1df6777dcb4918ef615f1799ee5606a2712886c0
Matthew <mattmuelle@gmail.com>
2016-12-30 02:22:13 -0600

    finish login/signup

--- pages/login.js
+++ pages/login.js
@@ -15,17 +15,16 @@ export default class Page extends Component {
       <div>
         <Head>
           <meta name='viewport' content='width=device-width, initial-scale=1' />
-          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+          <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet' />
           <link href='/static/tachyons.css' rel='stylesheet' />
           <link href='/static/flex.css' rel='stylesheet' />
           <link href='/static/global.css' rel='stylesheet' />
           <link href='/static/theme.css' rel='stylesheet' />
         </Head>
         <div>
-          <Portal ref='loginModal'>
-            <Login />
-          </Portal>
+          {/* <Portal ref='loginModal' /> */}
           <button className='ma5' onClick={() => this.refs.loginModal.openPortal()}>open login</button>
+          <Login />
         </div>
       </div>
     )
---
+++ pages/me.js
@@ -0,0 +1,21 @@
+import cookies from 'next-cookies'
+import { Component } from 'react'
+import graph from '../lib/graph'
+
+export default class Me extends Component {
+  static async getInitialProps (ctx) {
+    const { token } = cookies(ctx)
+
+  }
+
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render (props) {
+    return (
+      <div>me</div>
+    )
+  }
+}
