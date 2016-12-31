30c5e719582cd048ee80973987141f520becd73e
Michael Mueller <mdmueller3@wisc.edu>
2016-12-31 16:25:49 -0600

    Places images

--- components/header/index.js
+++ components/header/index.js
@@ -19,7 +19,8 @@ export default class Header extends Component {
     render (props) {
         return (
             <div className = "layout horizontal pa2 center mb0 sysFont" style = {{"height":"50px"}}>
-                <i className ="material-icons ph2 mid-gray">language</i>
+                {/* <i className ="material-icons ph2 mid-gray">language</i> */}
+                <img src = "/static/globe.svg" style = {{"height":"30px","margin-left":"25px","margin-right":"25px"}}/>
                 <div className = "b f5 mid-gray">COLUMBUS</div>
                 <div className = "ph3 f7 mid-gray" style = {{"marginLeft":"auto"}}>KURS EINSTELLEN</div>
                 <div className = "ph3 f7 mid-gray">HILFE</div>
--- pages/index.js
+++ pages/index.js
@@ -43,20 +43,62 @@ export default class Page extends Component {
             <div className="center" style = {{"textAlign":"center", "width":"80%","marginTop":"-50px", "maxWidth":"1200px"}}>
               <h1 className = "white mt0 normal headerSize mb1">Entdecke die Welt!</h1>
               <h2 className = "white mt0 normal f3 pv0 mb4">Finde den Kurs, der zu Dir passt!</h2>
-              <h3 className = "white mt0 normal f6 pv0 orange-yellow mb4">FINDE JETZT SPRACHKURSE, SPORTKURSE UND MEHR</h3>
+              <h3 className = "white mt0 normal f6 pv0 orange-yellow mb4" style = {{"letterSpacing":"2px"}}>FINDE JETZT SPRACHKURSE, SPORTKURSE UND MEHR</h3>
 
-              <div className = "bg-white mt0 layout horizontal searchBox" style = {{"width":"100%", "height":"80px"}}>
+              <div className = "bg-white mt0 center layout horizontal searchBox" style = {{"width":"100%", "height":"80px","maxWidth":"1000px"}}>
                 <div className = "flex layout vertical justify-center f6 pa3" style = {{"borderRight":"1px solid #eeeeee","textAlign":"left"}}><span className = "c-484848 pb2">Welche Art Kurs suchst du?</span><span className = "textSecondary">Sprachkurs, Tanzkurs, ...</span></div>
-                <div className = "flex layout vertical justify-center f6 pa3" style = {{"borderRight":"1px solid #eeeeee"}}><span className = "c-484848 pb2">Wo</span><span className = "textSecondary">Stadt, Land, Region</span></div>
-                <div className = "flex layout vertical justify-center f6 pa3"><span className = "c-484848 pb2">Wann</span><span className = "textSecondary">Datum oder Zeitraum</span></div>
-                <div className = "flex layout vertical justify-center"><button className = "btn">Suche</button></div>
+                <div className = "flex layout vertical justify-center f6 pa3" style = {{"borderRight":"1px solid #eeeeee","textAlign":"left"}}><span className = "c-484848 pb2">Wo</span><span className = "textSecondary">Stadt, Land, Region</span></div>
+                <div className = "flex layout vertical justify-center f6 pa3" style = {{"textAlign":"left"}}><span className = "c-484848 pb2">Wann</span><span className = "textSecondary">Datum oder Zeitraum</span></div>
+                <div className = "flex layout vertical justify-center pr3"><button className = "btn bn" style = {{"height":"40px","borderRadius":"5px","width":"150px"}}>Suche</button></div>
               </div>
 
             </div>
             <div style={{"paddingBottom":"38.2%"}} />
           </div>
 
+
+          {/*
+
+          <div className = "sysFont h5 w-80 center mt5 mb5" style = {{"border":"1px solid blue","textAlign":"center","paddingBottom":"40%","maxWidth":"1000px"}}>
+
+              <div className = "layout horizontal h-100">
+                <div className = "flex justify-center bg-red self-stretch">hi</div>
+                <div className = "flex justify-center bg-blue self-stretch">hello</div>
+              </div>
+
+ 
+
+
+          </div>
+
           {/* <img className="w-100 db" src="/static/berlin.jpg" /> /*}
+
+        */}
+
+        <div className='layout vertical tc mv5 ph7' style={{minHeight: '400px'}}>
+            <div className='layout horizontal flex-1'>
+              <div className='flex-2' style={{ backgroundImage: "url('/static/Header-bg.png')",backgroundSize:"cover"}}>paris</div>
+              <div className='layout vertical flex-1'>
+                <div className='flex-1' style={{ backgroundImage: "url('/static/Header-bg.png')",backgroundSize:"cover"}}>Rom</div>
+                <div className='flex-1' style={{ backgroundImage: "url('/static/Header-bg.png')",backgroundSize:"cover"}}>Lisabon</div>
+              </div>
+              <div className='layout vertical flex-1'>
+                <div className='flex-1' style={{ backgroundImage: "url('/static/Header-bg.png')",backgroundSize:"cover"}}>LA</div>
+                <div className='flex-1' style={{ backgroundImage: "url('/static/Header-bg.png')",backgroundSize:"cover"}}>Tokyo</div>
+              </div>
+            </div>
+            <div className='layout horizontal flex-1'>
+              <div className='layout vertical flex-1'>
+                <div className='flex-1' style={{ backgroundImage: "url('/static/Header-bg.png')",backgroundSize:"cover"}}>London</div>
+                <div className='flex-1' style={{ backgroundImage: "url('/static/Header-bg.png')",backgroundSize:"cover"}}>Barcelona</div>
+              </div>
+              <div className='flex-2' style={{ backgroundImage: "url('/static/Header-bg.png')",backgroundSize:"cover"}}>New York</div>
+              <div className='layout vertical flex-1'>
+                <div className='flex-1' style={{ backgroundImage: "url('/static/Header-bg.png')",backgroundSize:"cover"}}>Amsterdam</div>
+                <div className='flex-1' style={{ backgroundImage: "url('/static/Header-bg.png')",backgroundSize:"cover"}}>Berlin</div>
+              </div>
+            </div>
+          </div>
 
         {/*
 
