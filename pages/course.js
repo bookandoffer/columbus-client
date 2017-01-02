5366d1aef6a1486725eee5aed08ae6cac1d24b59
Matthew <mattmuelle@gmail.com>
2017-01-01 18:12:16 -0600

    load a course

--- pages/course.js
+++ pages/course.js
@@ -1,11 +1,41 @@
-
-import Head from '../components/head'
-import Portal from 'react-portal'
+import loadCourse from '../lib/load-course'
 import Header from '../components/header'
+import redirect from '../lib/redirect'
+import Head from '../components/head'
+import { Component } from 'react'
+import Router from 'next/router'
+import auth from '../lib/auth'
+import get from 'dlv'
 
-export default () => (
-  <div>
-    <Head title='Columbus | Course Details' />
-    <img width='112' className='pa5' src='https://cloud.githubusercontent.com/assets/13041/19686250/971bf7f8-9ac0-11e6-975c-188defd82df1.png' alt='next.js' />
-  </div>
-)
+const alert = typeof window === 'undefined'
+  ? console.error
+  : window.alert
+
+export default class Me extends Component {
+  static async getInitialProps (ctx) {
+    const courseId = get(ctx, 'query.id')
+
+    // go back if we don't have a course id via ?course=...
+    if (!courseId) return redirect(ctx, '/')
+
+    const res = await loadCourse(courseId)
+    if (res instanceof Error) {
+      alert(res.message)
+      return redirect(ctx, '/')
+    } else if (!res.course) {
+      alert('could not find the course you specified')
+      return redirect(ctx, '/')
+    }
+
+    return { course: res.course }
+  }
+
+  render () {
+    return (
+      <div>
+        <Head title='Columbus | Course Details' />
+        <pre><code>{JSON.stringify(this.props.course, true, 2)}</code></pre>
+      </div>
+    )
+  }
+}
