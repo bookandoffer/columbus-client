d3e6d7a6fba83c93724eb54a8a0defdadb4c23d7
Michael Mueller <mdmueller3@wisc.edu>
2017-01-15 10:53:09 -0600

    Fixed z-index issue with categories, added Other Courses button

--- pages/index.js
+++ pages/index.js
@@ -88,11 +88,11 @@ export default class Page extends Component {
         */}
 
         <div className = "flex w-70 m-auto pl2 mb4 mt6 helvetica c-484848" style = {{"maxWidth":"1000px"}}>Finde den richtigen Kurs für dich - einfach online</div>
-        <div className='mt3 mb5 layout w-70 m-auto horizontal wrap' style = {{"maxWidth":"1000px"}}>
+        <div className='mt3 mb3 layout w-70 m-auto horizontal wrap' style = {{"maxWidth":"1000px"}}>
           <div className='pa2 w-50'>
             <Link href='/courses?category=language'>
               <div className='aspect-ratio aspect-ratio--16x9  pointer'>
-                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ backgroundImage: `url(/static/categories/language.png)` }}>
+                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ zIndex: "0", backgroundImage: `url(/static/categories/language.png)` }}>
                   <h3 className='c-FFFFFF fw4 f3 mb0'>Sprachkurse</h3>
                   <p className='c-FFFFFF f6 fw2 mv1'>Englisch, Spanisch, Italienisch</p>
                 </div>
@@ -102,7 +102,7 @@ export default class Page extends Component {
           <div className='pa2 w-50'>
             <Link href='/courses?category=computer'>
               <div className='aspect-ratio aspect-ratio--16x9 pointer'>
-                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ backgroundImage: `url(/static/categories/computer.png)` }}>
+                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ zIndex: "0", backgroundImage: `url(/static/categories/computer.png)` }}>
                   <h3 className='c-FFFFFF fw4 f3 mb0'>Computerkurse</h3>
                   <p className='c-FFFFFF f6 fw2 mv1'>Word, Excel, iPad</p>
                 </div>
@@ -112,7 +112,7 @@ export default class Page extends Component {
           <div className='pa2 w-50'>
             <Link href='/courses?category=golf'>
               <div className='aspect-ratio aspect-ratio--16x9 pointer'>
-                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ backgroundImage: `url(/static/categories/golf.png)` }}>
+                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ zIndex: "0", backgroundImage: `url(/static/categories/golf.png)` }}>
                   <h3 className='c-FFFFFF fw4 f3 mb0'>Golfkurse</h3>
                   <p className='c-FFFFFF f6 fw2 mv1'>Platzreife, Fortgeschritten</p>
                 </div>
@@ -123,7 +123,7 @@ export default class Page extends Component {
           <div className='pa2 w-50'>
             <Link href='/courses?category=golf'>
               <div className='aspect-ratio aspect-ratio--16x9  pointer'>
-                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ backgroundImage: `url(/static/categories/painting.png)` }}>
+                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ zIndex: "0", backgroundImage: `url(/static/categories/painting.png)` }}>
                   <h3 className='c-FFFFFF fw4 f3 mb0'>Malkurse</h3>
                   <p className='c-FFFFFF f6 fw2 mv1'>Zeichnen für Einsteiger, Ölmalerei, Acryl</p>
                 </div>
@@ -133,7 +133,7 @@ export default class Page extends Component {
           <div className='pa2 w-50'>
             <Link href='/courses?category=painting'>
               <div className='aspect-ratio aspect-ratio--16x9  pointer'>
-                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ backgroundImage: `url(/static/categories/skiing.png)` }}>
+                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ zIndex: "0", backgroundImage: `url(/static/categories/skiing.png)` }}>
                   <h3 className='c-FFFFFF fw4 f3 mb0'>Skikurse</h3>
                   <p className='c-FFFFFF f6 fw2 mv1'>Einsteiger, Profis</p>
                 </div>
@@ -143,7 +143,7 @@ export default class Page extends Component {
           <div className='pa2 w-50'>
             <Link href='/courses?category=cooking'>
               <div className='aspect-ratio aspect-ratio--16x9  pointer'>
-                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ backgroundImage: `url(/static/categories/cooking.png)` }}>
+                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ zIndex: "0", backgroundImage: `url(/static/categories/cooking.png)` }}>
                   <h3 className='c-FFFFFF fw4 f3 mb0'>Kochkurse</h3>
                   <p className='c-FFFFFF f6 fw2 mv1'>Einsteiger, Fortgeschritten, Profis</p>
                 </div>
@@ -153,7 +153,7 @@ export default class Page extends Component {
           <div className='pa2 w-50'>
             <Link href='/courses?category=yoga'>
               <div className='aspect-ratio aspect-ratio--16x9  pointer'>
-                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ backgroundImage: `url(/static/categories/yoga.png)` }}>
+                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ zIndex: "0", backgroundImage: `url(/static/categories/yoga.png)` }}>
                   <h3 className='c-FFFFFF fw4 f3 mb0'>Yogakurse</h3>
                   <p className='c-FFFFFF f6 fw2 mv1'>Hatha Yoga, Einsteiger, Abendkurse</p>
                 </div>
@@ -163,7 +163,7 @@ export default class Page extends Component {
           <div className='pa2 w-50'>
             <Link href='/courses?category=chess'>
               <div className='aspect-ratio aspect-ratio--16x9  pointer'>
-                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ backgroundImage: `url(/static/categories/chess.png)` }}>
+                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ zIndex: "0", backgroundImage: `url(/static/categories/chess.png)` }}>
                   <h3 className='c-FFFFFF fw4 f3 mb0'>Schachkurse</h3>
                   <p className='c-FFFFFF f6 fw2 mv1'>Einsteiger, Fortgeschritten, Profis</p>
                 </div>
@@ -173,7 +173,7 @@ export default class Page extends Component {
           <div className='pa2 w-50'>
             <Link href='/courses?category=riding'>
               <div className='aspect-ratio aspect-ratio--16x9  pointer'>
-                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ backgroundImage: `url(/static/categories/riding.png)` }}>
+                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ zIndex: "0", backgroundImage: `url(/static/categories/riding.png)` }}>
                   <h3 className='c-FFFFFF fw4 f3 mb0'>Reitkurse</h3>
                   <p className='c-FFFFFF f6 fw2 mv1'>Kinderreitkurse, Einsteiger, Parcour</p>
                 </div>
@@ -184,7 +184,7 @@ export default class Page extends Component {
           <div className='pa2 w-50'>
             <Link href='/courses?category=fitness'>
               <div className='aspect-ratio aspect-ratio--16x9  pointer'>
-                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ backgroundImage: `url(/static/categories/fitness.png)` }}>
+                <div className='aspect-ratio--object pa3 cover layout vertical justify-end' style={{ zIndex: "0", backgroundImage: `url(/static/categories/fitness.png)` }}>
                   <h3 className='c-FFFFFF fw4 f3 mb0'>Fitnesskurse</h3>
                   <p className='c-FFFFFF f6 fw2 mv1'>Crossfit, Aerobic</p>
                 </div>
@@ -193,6 +193,9 @@ export default class Page extends Component {
           </div>
         </div>
 
+        <div className = "tc">
+          <Link href = "/categories"><button className='btn bn mb5 fw2 f6' style={{'height': '40px', 'borderRadius': '5px', 'width': '150px'}}>Weitere Kurse</button></Link>
+        </div>
 
         <div className='pa6 tl w-100 layout vertical mb5' style = {{backgroundColor:"#F7F9FB"}}>
           <p className = "f4 sysFont">Was ist BookAndOffer?</p>
