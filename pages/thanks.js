eb2d33c856a780770cbb55f628490ecfb25716ba
Marc Ft <marcftone@gmail.com>
2017-01-29 17:15:28 +0100

    fix thanks screen

--- pages/thanks.js
+++ pages/thanks.js
@@ -31,11 +31,6 @@ export default class Thanks extends Component {
             <div className='flex-1'>
               <h1 className='fw4'>Deine Buchungsanffage wurde übermittelt!</h1>
               <p>Der Gastgeber bzw. die Sprachschule wird sich innerhalb von 24 Studen bei dir melden und die Buchung bestätigen bzw. stornieren.</p>
-              <h2 className='fw4 mt5 mb0'>Standardkurs, NL, A1</h2>
-              <h3 className='silver f5 fw3 mt0 mb4'>Standardkurs, NL, A1</h3>
-              <div>12 Wochen à 20 Stunden</div>
-              <div>Startdatum: 01.02.2017</div>
-              <div>Gesamtsumme: 460 €</div>
             </div>
             <img className='flex-1' src='/static/backpack.svg' alt='Backpack' />
           </div>
